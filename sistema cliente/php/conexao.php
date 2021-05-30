ed181f89fde98ec871233a1747012b5a99b44c8f
Juliana de Souza <julianarodrigueslazer@gmail.com>
2021-05-30 19:28:16 -0300

    comecando o php

--- sistema cliente/php/conexao.php
+++ sistema cliente/php/conexao.php
@@ -1,14 +1,12 @@
 <?php
-comentario
    $servidor = "localhost";
     $usuario = "root";
     $senha = "";
     $database = "dbrestaurante";
-if ($conn = mysqli_connect($servidor, $usuario, $senha, $database)) {
-     //echo "ok";
-} else {
-        echo "deu merda";
+
+$conexao = new mysqli($servidor, $usuario, $senha, $database);
+
+if ($conexao->connect_error) {
+        die("Conexão inválida: " . $connect->connect_error);
 }
 
-
-?>
