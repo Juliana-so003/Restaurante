05e525b5fe66adcb37599080aba1dc4d4ff11183
Juliana-so003 <julianadesouzarodrigues100@gmail.com>
2021-05-31 20:42:39 -0300

    tirei o deu merda

--- sistema cliente/php/conexao.php
+++ sistema cliente/php/conexao.php
@@ -7,7 +7,7 @@
 if ($conn = mysqli_connect($servidor, $usuario, $senha, $database)) {
     // echo "ok";
 } else {
-        echo "deu merda";
+        echo "erro";
 }
 
 $conexao = new mysqli($servidor, $usuario, $senha, $database);
