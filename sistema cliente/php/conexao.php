671623f29bcec691e040cbaf3a7ecc006b48f4b1
Juliana <julianarodrigueslazer@gmail.com>
2021-06-06 10:55:30 -0300

    refazendo o banco de dados

--- sistema cliente/php/conexao.php
+++ sistema cliente/php/conexao.php
@@ -3,13 +3,7 @@
     $usuario = "root";
     $senha = "";
     $database = "dbrestaurante";
-
-if ($conn = mysqli_connect($servidor, $usuario, $senha, $database)) {
-    // echo "ok";
-} else {
-        echo "erroo";
-}
-
+    
 $conexao = new mysqli($servidor, $usuario, $senha, $database);
 
 if ($conexao->connect_error) {
