366940d3313e0ed3921825391769ffb965f0c87a
Juliana-so003 <julianadesouzarodrigues100@gmail.com>
2021-06-17 19:13:40 -0300

    jj

---
+++ sistema cliente/js/Cardapio_valor_aux.js
@@ -0,0 +1,33 @@
+
+const batata = document.querySelector("#batata");
+
+batata.addEventListener("click", function() {
+  document.getElementById('total').innerHTML = "R$ 20,90";
+
+  
+  
+});
+const camarao = document.querySelector("#camarao");
+
+camarao.addEventListener("click", function() {
+  document.getElementById('total').innerHTML = "R$ 55,90";
+
+  
+
+});
+const vinho = document.querySelector("#vinho");
+
+vinho.addEventListener("click", function() {
+  document.getElementById('total').innerHTML = "R$ 95,90";
+
+  
+
+});
+
+const cheesecake = document.querySelector("#cheesecake");
+
+cheesecake.addEventListener("click", function() {
+  document.getElementById('total').innerHTML = "R$ 125,90";
+
+
+});
