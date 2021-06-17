25e83410e3c107aaa45e1bd582a77d4fb4887a14
Juliana-so003 <julianadesouzarodrigues100@gmail.com>
2021-06-17 16:39:46 -0300

    arrumando

--- sistema cliente/js/Modal.js
+++ sistema cliente/js/Modal.js
@@ -20,6 +20,15 @@ document.body.style.overflow = 'hidden';
 function closeModal2(mn) {
   let modal = document.getElementById(mn);
   if (typeof modal == 'undefined' || modal === null) return;
+  
   modal.style.display = 'none';
   document.body.style.overflow = 'auto';
 }
+
+const voltar = document.querySelector(".voltar_login");
+
+
+
+voltar.addEventListener("click", function() {
+  window.location.href = 'Login.html';
+});
